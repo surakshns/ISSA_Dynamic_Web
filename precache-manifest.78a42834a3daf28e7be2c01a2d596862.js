524dd0800cc5b6934adf87a6ea5d2d8932b542ed
surakshns <surakshnss@gmail.com>
2020-10-09 14:33:12 +0530

    Updates

---
+++ precache-manifest.78a42834a3daf28e7be2c01a2d596862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc4306dad1fcbf83cee20c7090076a9",
+    "url": "/IssaDynamicWeb/index.html"
+  },
+  {
+    "revision": "d0ffa89033a41b31c2fd",
+    "url": "/IssaDynamicWeb/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "da550832947efae81e95",
+    "url": "/IssaDynamicWeb/static/css/main.dd421211.chunk.css"
+  },
+  {
+    "revision": "d0ffa89033a41b31c2fd",
+    "url": "/IssaDynamicWeb/static/js/2.8f43ad95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IssaDynamicWeb/static/js/2.8f43ad95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da550832947efae81e95",
+    "url": "/IssaDynamicWeb/static/js/main.a294bf79.chunk.js"
+  },
+  {
+    "revision": "cc7e9464ffe016d836a4",
+    "url": "/IssaDynamicWeb/static/js/runtime-main.4aa96fad.js"
+  }
+]);
