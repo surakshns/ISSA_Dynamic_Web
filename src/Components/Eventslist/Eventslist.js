56a05b184b06e168707f3f17a8d364fc453e4917
surakshns <surakshnss@gmail.com>
2020-11-15 14:05:17 +0530

    eve

--- src/Components/Eventslist/Eventslist.js
+++ src/Components/Eventslist/Eventslist.js
@@ -5,23 +5,20 @@ import Vid from './vid.mp4';
 
 class Eventslist extends Component{
 
-
-
-    
     render(){
         return(
             <div>
-                <div>
+                <div> 
                 <h1 className='eventheading'>Events</h1>
                 <video id="background-video" loop autoplay>
                 <source src={Vid} type="video/mp4" />
                 Your browser does not support the video tag.
                 </video>
                 </div>
-                <ul>{Elists.map((item, index) => {
+                <ul className="fulllist">{Elists.map((item, index) => {
                 return(
                 <li key={index}>
-                <div className="fulllist">
+                <div className="onecard">
                 <div class="card elistcard col-12">
                 <img class="card-img elistimg" src={item.image} alt="Card image cap" />
                 <div class="card-body">
